5cc682114d253e8e214c24da471e65f266bb424f
steve obongo <steveobongo0@gmail.com>
2025-08-08 20:31:26 +0300

    update: added fillable property

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'payment_id',
+        'payment_status',
+        'email_address',
+        'given_name',
+        'surname',
+        'currency_code',
+        'value'
+            
+    ];
 }
