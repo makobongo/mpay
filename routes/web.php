e6ca1e4aa50bd9b315b98f6d0a9fd6a4f00f1128
steve obongo <steveobongo0@gmail.com>
2025-08-08 13:07:44 +0300

    update: adjusted to grouped routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PaymentController::class, 'index'])->name('paypal.payment.index');
-Route::get('/payment/{id}', [PaymentController::class, 'PaypalPaymentLink'])->name('paypal.payment.payment');
-Route::get('/payment/success', [PaymentController::class, 'PaymentSuccess'])->name('paypal.payment.success');
-Route::get('/payment/cancel', [PaymentController::class, 'PaymentCancel'])->name('paypal.payment.cancel');
+Route::prefix('payment')->group(function () {
+    Route::post('/', [PaymentController::class, 'PaypalPaymentLink'])->name('paypal.payment.payment');
+    Route::get('/success', [PaymentController::class, 'PaymentSuccess'])->name('paypal.payment.success');
+    Route::get('/cancel', [PaymentController::class, 'PaymentCancel'])->name('paypal.payment.cancel');
+});
