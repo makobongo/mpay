b8469c7f64882b87621aaeb28d9c4de0afdc2f11
steve obongo <steveobongo0@gmail.com>
2025-08-08 11:10:10 +0300

    update: adjusted to named routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PaymentController::class, 'index']);
-Route::get('/payment/{id}', [PaymentController::class, 'PaypalPaymentLink']);
+Route::get('/', [PaymentController::class, 'index'])->name('paypal.payment.index');
+Route::get('/payment/{id}', [PaymentController::class, 'PaypalPaymentLink'])->name('paypal.payment.payment');
 Route::get('/payment/success', [PaymentController::class, 'PaymentSuccess'])->name('paypal.payment.success');
 Route::get('/payment/cancel', [PaymentController::class, 'PaymentCancel'])->name('paypal.payment.cancel');
