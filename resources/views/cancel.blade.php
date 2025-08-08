4ee4e2bae5f50e5a5364a33622259e60e5d091aa
steve obongo <steveobongo0@gmail.com>
2025-08-08 16:37:19 +0300

    update: adjusted to use primary extension

--- resources/views/cancel.blade.php
+++ resources/views/cancel.blade.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>MPAY</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
-<body>
-    <h3 style="text-align: center">
-        <b>Cancelled</b>
-    </h3>
-</body>
-</html>
+@extends('payment-status.app')
+@section('body')
+    <div class="card">
+        <div style="border-radius:200px; height:200px; width:200px; background: #F54927; margin:0 auto;">
+            <i class="checkmark">✖</i>
+        </div>
+        <h1>Cancelled</h1>
+        <p>Payment unsuccessful!</p>
+        <hr>
+        <a href="{{ route('paypal.payment.index') }}" target="_self">
+            <button style="background: #F54927;border:1px solid black;border-radius:10px;font-weight:bolder;cursor: pointer;">Back to home
+                page</button>
+        </a>
+    </div>
+@endsection
